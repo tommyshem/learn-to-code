0b2023aba8813134eb994cb2fbee22b6b31dfc84
tommyshem <1952659+tommyshem@users.noreply.github.com>
2019-10-13 16:52:56 +0100

    add more pointer types

--- learn-c/pointers/simple-pointer.c
+++ learn-c/pointers/simple-pointer.c
@@ -1,15 +1,20 @@
 #include "stdio.h"
 
 int main (){
-int a;
-int *p;
-p = &a;
-a =5;
+int a = 5;
+int *w; 	//wild pointer if it is not being initialized to anything. These types of pointers are not efficient because they may point to some unknown memory location
+void *x = NULL; 	//void pointer no data type can store anything but can crash.
+int *p = NULL; 	//null pointer
+p = &a;  //store address of variable a in pointer p
 printf("Value in A = %d\n",a);
 printf("Address of A = %p\n",&a);
+// Using pointers
+printf("The size of p pointer is: %lu\n",sizeof(p));
 printf("Address of P %p\n",&p);
-printf("P pointers to A address = %p\n",p);
-*p = 7;
+printf("P pointers to A address = %p\n",p);   //accessing the address
+printf("Value stored in a variable p is:%d\n",*p);   //accessing the value
+*p = 7;  // changing the value stored in A
+// Check value changed by using pointers
 printf("Value of A = %d\n",a);
 }
 
@@ -18,3 +23,8 @@ printf("Value of A = %d\n",a);
 //    P ----   |
 //             |
 //   *p -------
+
+// Notes
+// * 	Serves 2 purpose
+// int *p;                   Declaration of a pointer
+// a = *p;                   Returns the value of the referenced variable
