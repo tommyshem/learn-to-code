862f8d64c639be69a83c83cd80690fed50ee924e
Tommy <1952659+tommyshem@users.noreply.github.com>
2020-01-30 11:21:08 +0000

    part 3 - changed tuple to structure in the function

--- learn-rustlang/coding/the-rust-programming-language-book/minigrep/src/main.rs
+++ learn-rustlang/coding/the-rust-programming-language-book/minigrep/src/main.rs
@@ -1,29 +1,33 @@
 use std::env;
 use std::fs::File;
 use std::io::prelude::*;
-
 fn main() {
 	let args: Vec<String> = env::args().collect();
 	let command_name = &args[0];  //the first arg[0] is always the command path and name
-	let (search, filename) = parse_args(&args);   
+	let config = parse_args(&args);   
 
 	println!("\nCommand used {}",command_name);
-	println!("Searching for {}",search);
-	println!("In File {}\n",filename);
+	println!("Searching for {}",config.query);
+	println!("In File {}\n",config.filename);
 	
-	let mut f = File::open(filename).expect("File not found");
+	let mut f = File::open(config.filename).expect("File not found");
 	let mut contents = String::new();
 
 	f.read_to_string(&mut contents).expect("Something went wrong reading the file");
 	println!("With text:\n{}",contents);	
 }
 
+// config structure for holding the arguments passed in from the command line 
+struct Config{
+	query: String,  // owned string
+	filename: String,  // owned string
+}
 // parse command arguments passed in
 // args = args vector passed in
-// returns tuple of string slices as refs
-fn parse_args(args: &[String]) -> (&str,&str){
-	let query = &args[1];
-	let filename = &args[2];
-	(query,filename)
+// returns Config structure
+fn parse_args(args: &[String]) -> Config{
+	let query = args[1].clone();  // clone string so config struct as ownership
+	let filename = args[2].clone(); // clone string so config struct as ownership
+	Config {query,filename} // return config structure
 
 }
