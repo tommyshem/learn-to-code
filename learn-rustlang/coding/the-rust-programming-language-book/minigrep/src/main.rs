babba99790dd24ccf0a9469f0d95a67ee9db0abc
tommyshem <1952659+tommyshem@users.noreply.github.com>
2020-01-31 18:28:41 +0000

    changed error messages to standard error outout instead of standard output

--- learn-rustlang/coding/the-rust-programming-language-book/minigrep/src/main.rs
+++ learn-rustlang/coding/the-rust-programming-language-book/minigrep/src/main.rs
@@ -8,7 +8,7 @@ fn main() {
     let args: Vec<String> = env::args().collect(); // get arguments from the command line
     let command_name = &args[0]; //the first arg[0] is always the command path and name
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
+        eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
 
@@ -18,7 +18,7 @@ fn main() {
 
     if let Err(e) = minigrep::run(config) {
         // check if error return
-        println!("Application error: {}", e); // if error then print message and exit
+        eprintln!("Application error: {}", e); // if error then print message and exit
         process::exit(1); // exit program with error code 1
     }
 }
