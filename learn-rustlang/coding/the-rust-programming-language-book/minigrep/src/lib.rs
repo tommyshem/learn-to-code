b657045a05e6be7b693838acbcf7bdd60acd6113
Tommy <1952659+tommyshem@users.noreply.github.com>
2020-01-31 10:01:08 +0000

    move functions out of main in to lib file

---
+++ learn-rustlang/coding/the-rust-programming-language-book/minigrep/src/lib.rs
@@ -0,0 +1,37 @@
+use std::error::Error;
+use std::fs::File; // used for opening file
+use std::io::prelude::*;
+
+// config structure for holding the arguments passed in from the command line
+pub struct Config {
+    pub query: String,    // owned string
+    pub filename: String, // owned string
+}
+// parse command arguments passed in
+// args = args vector passed in
+// returns Config structure
+impl Config {
+    pub fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            // check there is 3 or more arguments passed in
+            return Err("Not enough arguments - need query and filename"); // if not enough arguments passed in
+        }
+        let query = args[1].clone(); // clone string so config struct as ownership
+        let filename = args[2].clone(); // clone string so config struct as ownership
+        Ok(Config { query, filename }) // return config structure
+    }
+}
+
+// config passed in and load the file
+// using ? to return error instead of panic with expect()
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    // result returns nothing and box returns any error
+    let mut f = File::open(config.filename)?; // try load file
+
+    let mut contents = String::new(); // owned string to load file contents into
+    f.read_to_string(&mut contents)?; // read file contents into owned string
+                                      // .expect("Something went wrong reading the file"); // if error panic and display message
+
+    println!("With text:\n{}", contents); // prinf contents to console output
+    Ok(()) // return ok result no return parameters
+}
--- learn-rustlang/coding/the-rust-programming-language-book/minigrep/src/main.rs
+++ learn-rustlang/coding/the-rust-programming-language-book/minigrep/src/main.rs
@@ -1,8 +1,8 @@
+extern crate minigrep;
+
 use std::env; // used to get arguments from command line
-use std::error::Error;
-use std::fs::File; // used for opening file
-use std::io::prelude::*;
 use std::process; // used for ending process // used for Error
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect(); // get arguments from the command line
@@ -16,43 +16,9 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In File {}\n", config.filename);
 
-    if let Err(e) = run(config) {
+    if let Err(e) = minigrep::run(config) {
         // check if error return
         println!("Application error: {}", e); // if error then print message and exit
         process::exit(1); // exit program with error code 1
-    }
-}
-
-// config passed in and load the file
-// using ? to return error instead of panic with expect()
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    // result returns nothing and box returns any error
-    let mut f = File::open(config.filename)?; // try load file
-
-    let mut contents = String::new(); // owned string to load file contents into
-    f.read_to_string(&mut contents)?; // read file contents into owned string
-                                      // .expect("Something went wrong reading the file"); // if error panic and display message
-
-    println!("With text:\n{}", contents); // prinf contents to console output
-    Ok(()) // return ok result no return parameters
-}
-
-// config structure for holding the arguments passed in from the command line
-struct Config {
-    query: String,    // owned string
-    filename: String, // owned string
-}
-// parse command arguments passed in
-// args = args vector passed in
-// returns Config structure
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            // check there is 3 or more arguments passed in
-            return Err("Not enough arguments - need query and filename"); // if not enough arguments passed in
-        }
-        let query = args[1].clone(); // clone string so config struct as ownership
-        let filename = args[2].clone(); // clone string so config struct as ownership
-        Ok(Config { query, filename }) // return config structure
     }
 }
