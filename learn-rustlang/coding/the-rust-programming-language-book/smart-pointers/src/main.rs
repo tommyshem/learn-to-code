2e19a4a3fef10bc8c7ee019796d3a0d853cee4b3
tommyshem <1952659+tommyshem@users.noreply.github.com>
2020-02-13 21:21:40 +0000

    drop trait for smartpointers

--- learn-rustlang/coding/the-rust-programming-language-book/smart-pointers/src/main.rs
+++ learn-rustlang/coding/the-rust-programming-language-book/smart-pointers/src/main.rs
@@ -1,3 +1,13 @@
+struct CustomSmartPointer {
+    data: String,
+}
+
+impl Drop for CustomSmartPointer {
+    fn drop(&mut self) {
+        println!("Dropping CustomSmartPointer with data {}", self.data);
+    }
+}
+
 fn main() {
     let a = 5; // stored on the stack
     let b = Box::new(a); // stored on the heap
@@ -7,4 +17,12 @@ fn main() {
     println!("a = {} ", a);
     println!("a = {} ", b); // will automatic dereferece *
     println!("b = {} ", *b); // same as above as
+
+    let c = CustomSmartPointer {
+        data: String::from("my stuff"),
+    };
+    let d = CustomSmartPointer {
+        data: String::from("other stuff"),
+    };
+    println!("CustomSmartPointers created");
 }
