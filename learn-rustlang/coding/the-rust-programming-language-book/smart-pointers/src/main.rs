6316b4bcebd5ca714425aaacaefb91ac6271ba84
tommyshem <1952659+tommyshem@users.noreply.github.com>
2020-02-13 21:01:32 +0000

    using box like a reference

--- learn-rustlang/coding/the-rust-programming-language-book/smart-pointers/src/main.rs
+++ learn-rustlang/coding/the-rust-programming-language-book/smart-pointers/src/main.rs
@@ -1,4 +1,10 @@
 fn main() {
-    let b = Box::new(5);
-    println!("b = {} ", b);
+    let a = 5; // stored on the stack
+    let b = Box::new(a); // stored on the heap
+    assert_eq!(5, a); // test value
+                      //    assert_eq!(5, b); // compile error if uncommented as need to dereference first * to get the value
+    assert_eq!(5, *b); // test value
+    println!("a = {} ", a);
+    println!("a = {} ", b); // will automatic dereferece *
+    println!("b = {} ", *b); // same as above as
 }
