f21c4effd76972cedd31c7370084d1b280319ffa
Tommy <1952659+tommyshem@users.noreply.github.com>
2020-02-15 14:19:02 +0000

    spelling correct and ignore cargo.lock file

--- learn-rustlang/coding/the-rust-programming-language-book/smart-pointers/src/main.rs
+++ learn-rustlang/coding/the-rust-programming-language-book/smart-pointers/src/main.rs
@@ -12,10 +12,10 @@ fn main() {
     let a = 5; // stored on the stack
     let b = Box::new(a); // stored on the heap
     assert_eq!(5, a); // test value
-                      //    assert_eq!(5, b); // compile error if uncommented as need to dereference first * to get the value
+                      //    assert_eq!(5, b); // compile error if uncomment as need to dereference first * to get the value
     assert_eq!(5, *b); // test value
     println!("a = {} ", a);
-    println!("a = {} ", b); // will automatic dereferece *
+    println!("a = {} ", b); // will automatic dereference *
     println!("b = {} ", *b); // same as above as
 
     let c = CustomSmartPointer {
