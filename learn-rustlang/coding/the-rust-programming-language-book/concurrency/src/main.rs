f15e3d2c02f9e434b13ee0f4e5046d9de9bfacb8
Tommy <1952659+tommyshem@users.noreply.github.com>
2020-03-08 01:21:30 +0000

    add concurrency functions examples

--- learn-rustlang/coding/the-rust-programming-language-book/concurrency/src/main.rs
+++ learn-rustlang/coding/the-rust-programming-language-book/concurrency/src/main.rs
@@ -1,3 +1,6 @@
+mod shared_state;
+
+use shared_state::mutex;
 use std::sync::mpsc;
 use std::thread; // threads
 use std::time::Duration; // time // channels
@@ -14,11 +17,58 @@ fn main() {
     // move closures in threads from pages 347-349
     println!("\nMove closure thread from pages 347-349");
     move_closure_threads();
-    // message passing between threads from pages 349-
-    println!("message passing between threads from pages 349-352");
+    // message passing between threads from pages 349-352
+    println!("\nmessage passing between threads from pages 349-352");
     message_passing_between_threads();
-    println!("message passing between threads sending multiple values from pages 353-");
+    // message passing multiple values from pages 353-354
+    println!("\nmessage passing between threads sending multiple values from pages 353-354");
     message_passing_between_threads_sending_multiple_values();
+    // message passing multiple values using clone from pages 354-355
+    println!("\nmessage passing between threads sending multiple values using clone");
+    message_passing_between_threads_sending_multiple_values_using_clone();
+    // mutex in single thread from page 356
+    println!("\nmutex in single thread from page 356");
+    shared_state::mutex::mutex_example();
+    // sharing mutex between multiple threads from pages 357-
+    println!("sharing mutex between multiple threads from pages 357-");
+    shared_state::mutex::sharing_mutex_between_threads_example();
+}
+
+/// message passing between threads using multiple values using clone
+fn message_passing_between_threads_sending_multiple_values_using_clone() {
+    let (tx, rx) = mpsc::channel();
+    let tx1 = mpsc::Sender::clone(&tx);
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+        for val in vals {
+            tx1.send(val).unwrap();
+            thread::sleep(Duration::from_millis(3));
+        }
+    });
+
+    thread::spawn(move || {
+        let vals1 = vec![
+            String::from("more"),
+            String::from("messages"),
+            String::from("for"),
+            String::from("you"),
+        ];
+        for val1 in vals1 {
+            tx.send(val1).unwrap();
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+
+    for received in rx {
+        // [blocked] wait until the thread drops the channel
+        println!("Got: {}", received);
+    }
 }
 
 /// message passing between threads using multiple values
@@ -37,6 +87,7 @@ fn message_passing_between_threads_sending_multiple_values() {
         }
     });
     for received in rx {
+        // [blocked] wait until the thread drops the channel
         println!("Got: {}", received);
     }
 }
