381e306b2e04efdab6333622f5787e680ec0b854
Tommy <1952659+tommyshem@users.noreply.github.com>
2020-03-09 18:26:10 +0000

    let example

---
+++ learn-rustlang/coding/the-rust-programming-language-book/pattern_matching/let_example/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    // match tuple to variable names
+    let (x, y, z) = (1, 2, 3);
+    // print to stdout using the named variables
+    println!("X: {}", x);
+    println!("Y: {}", y);
+    println!("Z: {}", z);
+}
