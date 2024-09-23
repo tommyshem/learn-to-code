2cee0985ac71cd129e4bbe43aa1537017110d8d6
Tommy <1952659+tommyshem@users.noreply.github.com>
2024-09-23 21:03:48 +0100

    add more examples from the book

---
+++ learn-c/practical-C/example-5/7/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/* 
+*	EXAMPLE 5-7
+*	Example how to use with multidimensional arrays
+*/
+
+/* variables */
+char number_array[3][2]; /* Array of numbers */
+
+int main(){
+	/* Full in number array with data */
+	number_array[0][0] = 0 * 10 + 0;
+	number_array[0][1] = 0 * 10 + 1;
+	number_array[1][0] = 1 * 10 + 0;
+	number_array[1][1] = 1 * 10 + 1;
+	number_array[2][0] = 2 * 10 + 0;
+	number_array[2][1] = 2 * 10 + 1;
+	/* Print number array 0*/
+	printf("number array [0] [0] = %d \n",number_array[0][0]);
+	printf("number array [0] [1] = %d \n", number_array[0][1]);
+	/* Print number array 1 */
+	printf("number array [1] [0] = %d \n",number_array[1][0]);
+	printf("number array [1] [1] = %d \n", number_array[1][1]);
+	/* Print number array 2*/
+	printf("number array [2] [0] = %d \n",number_array[2][0]);
+	printf("number array [2] [1] = %d \n", number_array[2][1]);
+	return 0;
+}
