a878217a360e04a8771ba4fc0d704128c47a535f
tommy shem <tommy@test.com>
2025-08-12 11:13:17 +0100

    added records and structs

---
+++ Learning_C_Sharp/CSharpPLayersGuide-5/CSharpPLayersGuide-5/Records.cs
@@ -0,0 +1,5 @@
+namespace Learning_C_Sharp;
+// records are compact alterative notation for defining a data-centric
+// class or struct. Compiler automaticaly generates constructor and properties
+// and tostring 
+public record Points(float x, float y);
--- Learning_C_Sharp/CSharpPLayersGuide-5/CSharpPLayersGuide-5/struct.cs
+++ Learning_C_Sharp/CSharpPLayersGuide-5/CSharpPLayersGuide-5/struct.cs
@@ -1,11 +1,23 @@
 namespace Learning_C_Sharp;
 
-public struct Point
+// structs are value types not reference types like classes
+// structs are primarily useful for small data-related concepts
+// structs are automatically initialized to default values
+// structs should be immutable ie no setters
+// primary short hand for struct if only 1 constructor is used
+public struct Point(float x, float y)
+{
+    public float X { get; } = x;
+    public float Y { get; } = y;
+}
+
+// same as above but long hand way and can have different constructors
+public struct Point1
 {
     public float X { get; }
     public float Y { get; }
 
-    public Point(float x, float y)
+    public Point1(float x, float y)
     {
         X = x;
         Y = y;
@@ -14,6 +26,10 @@ public struct Point
 
 public class structExample()
 {
-    private Point p1 = new Point(2, 4);
-   // Console.WriteLine($"({p1.X}, {p1.Y}");
+    private Point _p1 = new Point(2, 4);
+    //Console.WriteLine($"({p1.X}, {p1.Y}");
+    // boxing
+    static Object _o = 3;
+    // unboxing
+    int p = (int)_o;
 }
